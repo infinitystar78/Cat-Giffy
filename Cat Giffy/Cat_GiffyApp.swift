4c2df1c9bc2bc00ffc861efbddf1852a60f812fd
Melvin <melvinjw@me.com>
2022-01-17 23:27:20 +0000

    Initial Commit

---
+++ Cat Giffy/Cat_GiffyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cat_GiffyApp.swift
+//  Cat Giffy
+//
+//  Created by M W on 17/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct Cat_GiffyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cat GiffyUITests/Cat_GiffyUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Cat_GiffyUITestsLaunchTests.swift
+//  Cat GiffyUITests
+//
+//  Created by M W on 17/01/2022.
+//
+
+import XCTest
+
+class Cat_GiffyUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
